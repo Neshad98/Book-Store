954a3ab8a330e58109ffdabe9a524dd919f2342d
Neshad <djneshad@gmail.com>
2024-01-23 21:16:18 +0600

    axios error fixed

--- backend/index.js
+++ backend/index.js
@@ -14,15 +14,15 @@ const app = express();
 
 //middleware
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
-// app.use(
-//   cors({
-//     origin: 'http://localhost:5000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-//   })
-// );
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
 
 app.get('/', (req, res) => {
   // console.log(req);
