b13515ea9383d5977bdc69cd8013af96fc1494fa
Neshad <djneshad@gmail.com>
2024-01-26 19:43:17 +0600

    working

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react"
 
 
 const CreateBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
   return (
     <div>CreateBook</div>
   )
