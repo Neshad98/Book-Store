1a0fb46ad8b88b6d6a625fba4c4bb94165ede18e
Neshad <djneshad@gmail.com>
2024-01-26 19:47:14 +0600

    working

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,10 +1,26 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
 
 
 const CreateBook = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios.post('http://localhost:5000/books', data)
+  }
+
   return (
     <div>CreateBook</div>
   )
